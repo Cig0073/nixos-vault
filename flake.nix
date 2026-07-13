0090f8fbc4ac61ca2ec69c001cf338daeb05ebe6
cig0073 <cig0073@protonmail.com>
2026-07-13 21:57:59 +0300

    update to flakes

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A clean, modular NixOS system configuration flake";
+
+   inputs = {
+     # Using unstable by default; switch to a specific channel like "nixos-24.05" if preferred
+     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+   };
+
+   outputs = { self, nixpkgs, ... }@inputs: {
+     nixosConfigurations = {
+	   nixos-vault = nixpkgs.lib.nixosSystem {
+	     system = "x86_64-linux";
+	     specialArgs = { inherit inputs; }; 
+         modules = [ ./configuration.nix ];
+	  };
+	};
+  };
+}
